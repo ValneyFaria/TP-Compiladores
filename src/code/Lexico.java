f818a0d4f2424a435a50f819629060a4c8f296a2
Valney Faria <valneyfaria@hotmail.com>
2018-08-09 18:56:03 -0300

    Tabela de Lexemas Inserida

--- src/code/Lexico.java
+++ src/code/Lexico.java
@@ -1,16 +1,16 @@
 package code;
 
-import java.awt.List;
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class Lexico {
 
 	public static void main(String[] args) {
-		
+
 		// Tabela de Lexemas
 		HashMap<String, String> Lexemas = new HashMap<String, String>();
-		
 
 		Lexemas.put("main", "MAIN");
 		Lexemas.put("int", "INT");
@@ -40,13 +40,21 @@ public class Lexico {
 		Lexemas.put("int", "INT");
 		Lexemas.put("int", "INT");
 
-		/*[A-Za-z] ([A-Za-z] | [0-9] | _ )*
-		[0-9] ([0-9])*
-		[0-9] ([0-9])*.[0-9] ([0-9])*
+		/*
+		 * [A-Za-z] ([A-Za-z] | [0-9] | _ )* [0-9] ([0-9])* [0-9] ([0-9])*.[0-9]
+		 * ([0-9])*
+		 * 
+		 * 
+		 */
 
-
-		*/
-		
+		/* Exibe conteudo usando Iterator */
+		Set set = Lexemas.entrySet();
+		Iterator iterator = set.iterator();
+		while (iterator.hasNext()) {
+			Map.Entry mentry = (Map.Entry) iterator.next();
+			System.out.print("CHAVE: " + mentry.getKey() + " - VALOR: ");
+			System.out.println(mentry.getValue());
+		}
 
 		System.out.println("oi");
 
