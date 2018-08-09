363f62361c8a2c59e5fa04c866e529cce124bf49
Valney Faria <valneyfaria@hotmail.com>
2018-08-09 17:28:31 -0300

    Commit Inicial

---
+++ src/code/Lexico.java
@@ -0,0 +1,19 @@
+package code;
+
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Lexico {
+
+	public static void main(String[] args) {
+		
+		// Tabela de Lexemas
+		HashMap<String, String> Lexemas = new HashMap<String, String>();
+		
+		
+
+		System.out.println("oi");
+
+	}
+}
