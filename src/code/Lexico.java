bc110247e15c9b992568a7513c0828d559982c19
Valney Faria <valneyfaria@hotmail.com>
2018-08-09 18:40:57 -0300

    Tabela de Lexemas Inserida

--- src/code/Lexico.java
+++ src/code/Lexico.java
@@ -11,6 +11,41 @@ public class Lexico {
 		// Tabela de Lexemas
 		HashMap<String, String> Lexemas = new HashMap<String, String>();
 		
+
+		Lexemas.put("main", "MAIN");
+		Lexemas.put("int", "INT");
+		Lexemas.put("float", "FLOAT");
+		Lexemas.put("if", "IF");
+		Lexemas.put("else", "ELSE");
+		Lexemas.put("while", "WHILE");
+		Lexemas.put("for", "FOR");
+		Lexemas.put("read", "READ");
+		Lexemas.put("(", "LBRACKET");
+		Lexemas.put(")", "RBRACKET");
+		Lexemas.put("{", "LBRACE");
+		Lexemas.put("}", "RBRACE");
+		Lexemas.put(",", "COMMA");
+		Lexemas.put(";", "PCOMMA");
+		Lexemas.put("=", "ATTR");
+		Lexemas.put("<", "LT");
+		Lexemas.put("<=", "LE");
+		Lexemas.put(">", "GT");
+		Lexemas.put(">=", "GE");
+		Lexemas.put("+", "PLUS");
+		Lexemas.put("-", "MINUS");
+		Lexemas.put("*", "MULT");
+		Lexemas.put("/", "DIV");
+		Lexemas.put("int", "INT");
+		Lexemas.put("int", "INT");
+		Lexemas.put("int", "INT");
+		Lexemas.put("int", "INT");
+
+		/*[A-Za-z] ([A-Za-z] | [0-9] | _ )*
+		[0-9] ([0-9])*
+		[0-9] ([0-9])*.[0-9] ([0-9])*
+
+
+		*/
 		
 
 		System.out.println("oi");
